778fbcba656d6e2370d4e5fadea2b27557be7255
Gonzalo Alfredo Martone <gmartone@itba.edu.ar>
2026-03-03 16:05:11 -0300

    feat: inmemoryrepository

--- src/main/java/com/gmartone/mendel/transactions/InMemoryTransactionsRepository.java
+++ src/main/java/com/gmartone/mendel/transactions/InMemoryTransactionsRepository.java
@@ -1,28 +1,47 @@
 package com.gmartone.mendel.transactions;
 
 import com.gmartone.mendel.transactions.dto.Transaction;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Repository
 public class InMemoryTransactionsRepository implements TransactionsRepository {
+
+    private final Map<Long, Transaction> byId = new HashMap<>();
+    private final Map<String, Set<Long>> byType = new HashMap<>();
+    private final Map<Long, Set<Long>> childrenByParent = new HashMap<>();
+
     @Override
     public Transaction findById(long id) {
-        return null;
+        return byId.get(id);
     }
 
     @Override
     public Transaction create(Transaction transaction) {
-        return null;
+
+        byId.put(transaction.id(), transaction);
+
+        // index by type
+        byType
+                .computeIfAbsent(transaction.type(), k -> new HashSet<>())
+                .add(transaction.id());
+
+        // index by parent
+        if (transaction.parent_id() != null) {
+            childrenByParent
+                    .computeIfAbsent(transaction.parent_id(), k -> new HashSet<>())
+                    .add(transaction.id());
+        }
+
+        return transaction;
     }
 
-    @Override
     public List<Long> findByType(String type) {
-        return List.of();
+        return byType.getOrDefault(type, Collections.emptySet()).stream().toList();
     }
 
-    @Override
-    public Set<Long> findChildren(long id) {
-        return Set.of();
+    public Set<Long> findChildren(long parentId) {
+        return childrenByParent.getOrDefault(parentId, Collections.emptySet());
     }
 }
